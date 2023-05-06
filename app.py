23c43da2e55f31937a24f76bf77880c1265b00e8
tekelala <camilo@tekelala.com>
2023-05-06 10:49:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,50 @@
 import streamlit as st
 import requests
+import json
 
 # Define the Claude API endpoint 
 API_ENDPOINT = "https://api-inference.huggingface.co/models/claude-cpc:latest"
 
-# Define a function to get a response from Claude
+# Define a function to query Claude
 def query(payload):
   response = requests.post(API_ENDPOINT, headers={"Content-Type": "application/json"}, data=payload)
-  response = response.json()
+  
+  # Handle response and errors
+  try: 
+    response = response.json()
+  except:
+    st.error("Invalid JSON response from API")
+    st.stop()
+    
+  # Check if "responses" key exists
+  if "responses" not in response:
+    st.error("Invalid JSON response from API")
+    st.stop()
+    
   return response
 
-# Create a title and intro on the page 
+# Create page title and intro
 st.title("Chatbot")
 st.write("This is a basic chatbot using the Claude API.")
 
-# Prompt the user to enter a query 
-query_input = st.text_input("You: ", "How can I help you today?")
+# Prompt user for query and call API  
+query_input = st.text_input("You: ", "How can I help you today?") 
+response = query({ "inputs": query_input })
 
-# Call the Claude API with the user's query 
-response = query({
-  "inputs": query_input, 
-})
-
-# Display Claude's response
-st.write("Claude: ",  response["responses"][0]["text"])
-
-# Add interactivity by continuously prompting for new queries 
-while True: 
+# Display response from API 
+try: 
+  st.write("Claude: ", response["responses"][0]["text"]) 
+except:
+  st.error("Unable to display response from API")
+  st.stop()
+  
+# Continuously prompt for queries 
+while True:    
   query_input = st.text_input("You: ", "")
-  if query_input: 
-    response = query({
-      "inputs": query_input, 
-    })
-    st.write("Claude: ",  response["responses"][0]["text"])
+  if query_input:
+    response = query({ "inputs": query_input })
+    try: 
+      st.write("Claude: ", response["responses"][0]["text"])
+    except:
+      st.error("Unable to display response from API")
+      st.stop() 
