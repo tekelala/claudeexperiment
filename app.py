210eb11b418debffcd495495a5349cac93f288c9
tekelala <camilo@tekelala.com>
2023-05-06 11:25:39 -0500

    another try

--- app.py
+++ app.py
@@ -5,29 +5,30 @@ import json
 st.title("Chat with Claude")
 
 # Define initial prompts
-prompts = []
+if "prompts" not in st.session_state:
+    st.session_state.prompts = []
 
 # Fetch user input
 user_input = st.text_input("You: ")
 
 # If there's user input, append it to the prompts
 if user_input:
-    prompts.append({
+    st.session_state.prompts.append({
         "role": "Human",
         "content": user_input
     })
 
 # When the user clicks "Send", make a request to Claude API
 if st.button("Send"):
-    if prompts:
+    if st.session_state.prompts:
         api_url = "https://api.anthropic.com/v1/complete"
         headers = {
             "Content-Type": "application/json",
-            "X-API-Key": "API_KEY"
+            "X-API-Key": st.secrets["API_KEY"]  # Use the API key from Streamlit's secrets
         }
 
         # Prepare the prompts for Claude
-        conversation = "\n\n".join([f'{item["role"]}: {item["content"]}' for item in prompts]) + "\n\nAssistant:"
+        conversation = "\n\n".join([f'{item["role"]}: {item["content"]}' for item in st.session_state.prompts]) + "\n\nAssistant:"
 
         # Define the body of the request
         body = {
@@ -38,19 +39,23 @@ if st.button("Send"):
         }
 
         # Make a POST request to the Claude API
-        response = requests.post(api_url, headers=headers, data=json.dumps(body))
+        try:
+            response = requests.post(api_url, headers=headers, data=json.dumps(body))
+            response.raise_for_status()
 
-        # If the request is successful, display the response
-        if response.status_code == 200:
             result = response.json()
             st.text("Claude: " + result['completion'])
 
             # Append Claude's response to the prompts
-            prompts.append({
+            st.session_state.prompts.append({
                 "role": "Assistant",
                 "content": result['completion']
             })
-        else:
-            st.error("There was an error making the request.")
-
-st.session_state.prompts = prompts
+        except requests.exceptions.HTTPError as errh:
+            st.error(f"HTTP Error: {errh}")
+        except requests.exceptions.ConnectionError as errc:
+            st.error(f"Error Connecting: {errc}")
+        except requests.exceptions.Timeout as errt:
+            st.error(f"Timeout Error: {errt}")
+        except requests.exceptions.RequestException as err:
+            st.error(f"Something went wrong: {err}")
