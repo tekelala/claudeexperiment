4665c74abade5f5506839ee91c22b4ae32e7bc78
tekelala <camilo@tekelala.com>
2023-05-06 19:29:06 -0500

    version updated 2

--- app.py
+++ app.py
@@ -2,21 +2,13 @@ import streamlit as st
 import requests
 import json
 
-st.title("Chat with Claude")
+def display_app():
+    # Container for Title and Banner
+    with st.container():
+        st.title("Chat with Claude")
+        st.write("Welcome to our chat app!")  # Welcome message
+        # st.image("banner.jpg")  # Display a banner (uncomment this line and replace "banner.jpg" with the path to your banner image)
 
-# Container for welcome message and banner
-with st.container():
-    st.write("Welcome to our chat app!")  # Welcome message
-    # st.image("banner.jpg")  # Display a banner (uncomment this line and replace "banner.jpg" with the path to your banner image)
-
-# Define initial prompts
-if "prompts" not in st.session_state:
-    st.session_state.prompts = []
-
-# Placeholder for user input
-user_input = st.empty()
-
-def display_chat_history():
     # Container for conversation history
     with st.container():
         # Display the entire conversation
@@ -26,63 +18,65 @@ def display_chat_history():
             else:  # prompt['role'] == 'Assistant'
                 st.write(f"Claude: {prompt['content']}")
 
-# Display the chat history at the start
-display_chat_history()
+    # Container for user input and Send button
+    with st.container():
+        user_message = st.text_input("You: ")
+        if st.button("Send"):
+            if user_message:
+                st.session_state.prompts.append({
+                    "role": "Human",
+                    "content": user_message
+                })
 
-# User input and Send button
-user_message = user_input.text_input("You: ")
+                if st.session_state.prompts:
+                    api_url = "https://api.anthropic.com/v1/complete"
+                    headers = {
+                        "Content-Type": "application/json",
+                        "X-API-Key": st.secrets["API_KEY"]  # Use the API key from Streamlit's secrets
+                    }
 
-# When the user clicks "Send", append the input to the prompts and make a request to Claude API
-if st.button("Send"):
-    if user_message:
-        st.session_state.prompts.append({
-            "role": "Human",
-            "content": user_message
-        })
+                    # Prepare the prompts for Claude
+                    conversation = "\n\n".join([f'{item["role"]}: {item["content"]}' for item in st.session_state.prompts]) + "\n\nAssistant:"
 
-        if st.session_state.prompts:
-            api_url = "https://api.anthropic.com/v1/complete"
-            headers = {
-                "Content-Type": "application/json",
-                "X-API-Key": st.secrets["API_KEY"]  # Use the API key from Streamlit's secrets
-            }
+                    # Define the body of the request
+                    body = {
+                        "prompt": conversation,
+                        "model": "claude-v1.3",
+                        "max_tokens_to_sample": 100,
+                        "stop_sequences": ["\n\nHuman:"]
+                    }
 
-            # Prepare the prompts for Claude
-            conversation = "\n\n".join([f'{item["role"]}: {item["content"]}' for item in st.session_state.prompts]) + "\n\nAssistant:"
+                    # Make a POST request to the Claude API
+                    with st.spinner('Waiting for Claude...'):
+                        try:
+                            response = requests.post(api_url, headers=headers, data=json.dumps(body))
+                            response.raise_for_status()
 
-            # Define the body of the request
-            body = {
-                "prompt": conversation,
-                "model": "claude-v1.3",
-                "max_tokens_to_sample": 100,
-                "stop_sequences": ["\n\nHuman:"]
-            }
+                            result = response.json()
 
-            # Make a POST request to the Claude API
-            with st.spinner('Waiting for Claude...'):
-                try:
-                    response = requests.post(api_url, headers=headers, data=json.dumps(body))
-                    response.raise_for_status()
+                            # Append Claude's response to the prompts
+                            st.session_state.prompts.append({
+                                "role": "Assistant",
+                                "content": result['completion']
+                            })
 
-                    result = response.json()
+                            # Clear the text input box
+                            st.text_input("You: ", value="", key="unique")
 
-                    # Append Claude's response to the prompts
-                    st.session_state.prompts.append({
-                        "role": "Assistant",
-                        "content": result['completion']
-                    })
-                    
-                    # Display the updated chat history
-                    display_chat_history()
+                            # Redraw the entire app to reflect the new chat history
+                            display_app()
 
-                except requests.exceptions.HTTPError as errh:
-                    st.error(f"HTTP Error: {errh}")
-                except requests.exceptions.ConnectionError as errc:
-                    st.error(f"Error Connecting: {errc}")
-                except requests.exceptions.Timeout as errt:
-                    st.error(f"Timeout Error: {errt}")
-                except requests.exceptions.RequestException as err:
-                    st.error(f"Something went wrong: {err}")
+                        except requests.exceptions.HTTPError as errh:
+                            st.error(f"HTTP Error: {errh}")
+                        except requests.exceptions.ConnectionError as errc:
+                            st.error(f"Error Connecting: {errc}")
+                        except requests.exceptions.Timeout as errt:
+                            st.error(f"Timeout Error: {errt}")
+                        except requests.exceptions.RequestException as err:
+                            st.error(f"Something went wrong: {err}")
 
-    # Clear the text input box
-    user_input.text_input("You: ", value="", key="unique")
+# Define initial prompts
+if "prompts" not in st.session_state:
+    st.session_state.prompts = []
+
+display_app()
