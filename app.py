b52411128e8ac238723affd3676532b0f749b60f
tekelala <camilo@tekelala.com>
2023-05-06 19:41:57 -0500

    v2

--- app.py
+++ app.py
@@ -2,6 +2,30 @@ import streamlit as st
 import requests
 import json
 
+def send_message(prompts):
+    api_url = "https://api.anthropic.com/v1/complete"
+    headers = {
+        "Content-Type": "application/json",
+        "X-API-Key": st.secrets["API_KEY"]  # Use the API key from Streamlit's secrets
+    }
+
+    # Prepare the prompts for Claude
+    conversation = "\n\n".join([f'{item["role"]}: {item["content"]}' for item in prompts]) + "\n\nAssistant:"
+
+    # Define the body of the request
+    body = {
+        "prompt": conversation,
+        "model": "claude-v1.3",
+        "max_tokens_to_sample": 100,
+        "stop_sequences": ["\n\nHuman:"]
+    }
+
+    # Make a POST request to the Claude API
+    response = requests.post(api_url, headers=headers, data=json.dumps(body))
+    response.raise_for_status()
+
+    return response.json()
+
 # Container for Title and Banner
 with st.container():
     st.title("Chat with Claude")
@@ -27,38 +51,16 @@ with st.container():
         user_message = st.text_input("You: ", key=f"user_input_{len(st.session_state.prompts)}")
         submit_button = st.form_submit_button(label='Send')
 
-    if submit_button:
-        if user_message:
+        if submit_button and user_message:
             st.session_state.prompts.append({
                 "role": "Human",
                 "content": user_message
             })
 
             if st.session_state.prompts:
-                api_url = "https://api.anthropic.com/v1/complete"
-                headers = {
-                    "Content-Type": "application/json",
-                    "X-API-Key": st.secrets["API_KEY"]  # Use the API key from Streamlit's secrets
-                }
-
-                # Prepare the prompts for Claude
-                conversation = "\n\n".join([f'{item["role"]}: {item["content"]}' for item in st.session_state.prompts]) + "\n\nAssistant:"
-
-                # Define the body of the request
-                body = {
-                    "prompt": conversation,
-                    "model": "claude-v1.3",
-                    "max_tokens_to_sample": 100,
-                    "stop_sequences": ["\n\nHuman:"]
-                }
-
-                # Make a POST request to the Claude API
                 with st.spinner('Waiting for Claude...'):
                     try:
-                        response = requests.post(api_url, headers=headers, data=json.dumps(body))
-                        response.raise_for_status()
-
-                        result = response.json()
+                        result = send_message(st.session_state.prompts)
 
                         # Append Claude's response to the prompts
                         st.session_state.prompts.append({
@@ -69,6 +71,9 @@ with st.container():
                         # Rerun the script to update the chat
                         st.experimental_rerun()
 
+                        # Display a success message
+                        st.success("Message sent successfully!")
+
                     except requests.exceptions.HTTPError as errh:
                         st.error(f"HTTP Error: {errh}")
                     except requests.exceptions.ConnectionError as errc:
@@ -77,3 +82,5 @@ with st.container():
                         st.error(f"Timeout Error: {errt}")
                     except requests.exceptions.RequestException as err:
                         st.error(f"Something went wrong: {err}")
+                    except Exception as e:
+                        st.error(f"Unexpected error: {e}")
